a4a30112f24e27ec98592c093085572b7211f073
Svetozara3363 <120119368+Svetozara3363@users.noreply.github.com>
2023-11-28 17:23:15 +0300

    Initial Commit

---
+++ News IT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  News IT
+//
+//  Created by svetozara on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ News IT/News_ITApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_ITApp.swift
+//  News IT
+//
+//  Created by svetozara on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct News_ITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
